2ca8ef9df97e8ea61e3dda402aba1cb5ff654a08
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2025-09-12 19:37:50 +0530

    Client Setup

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Sidebar from '../components/Sidebar';
 
 const Layout = () => {
   return (
-    <div>Layout</div>
-  )
+    <div className="w-full flex h-screen">
+    <Sidebar/>
+    <div>
+      
+    </div>
+      <h1>Layout</h1>
+    </div>
+  );
 }
 
 export default Layout
