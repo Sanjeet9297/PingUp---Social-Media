835ca57ca86061766d31aa284ca35536c2501e1a
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2025-09-16 18:40:12 +0530

    List post cmplt

--- client/src/pages/Feed.jsx
+++ client/src/pages/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import StoriesBar from "../components/StoriesBar";
+import PostCard from "../components/PostCard";
 
 const Feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -26,7 +27,9 @@ const Feed = () => {
       <div>
      <StoriesBar/>
       <div className="p-4 space-y-6">
-        List of post
+        {feeds.map((post) =>(
+          <PostCard  key={post._id} post={post}/>
+        ))}
       </div>
       </div>
 
