18f1d528e46550b78260103cc7dd5082c85e06c6
Sanjeet Kumar <oksanjeetkr124@gmail.com>
2025-09-19 18:19:18 +0530

    new

--- server/server.js
+++ server/server.js
@@ -12,11 +12,29 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// ✅ Add CSP headers
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    [
+      "default-src 'self'", 
+      "script-src 'self' 'unsafe-eval' 'unsafe-inline'", 
+      "style-src 'self' 'unsafe-inline'", 
+      "img-src 'self' data: https:", 
+      "font-src 'self' https:", 
+      "connect-src 'self' https:", 
+      "frame-src 'self' https:", 
+    ].join("; ")
+  );
+  next();
+});
+
 app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
   res.send("Server is Live!");
 });
+
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/user", userRouter);
 
